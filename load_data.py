940acd786ca6996e379355756a8059cb7729e25c
michael.cunha <mike@MikeBook-Air.local>
2018-02-20 20:08:20 -0500

    Update to Python 3
    Using 2to3.

--- load_data.py
+++ load_data.py
@@ -6,16 +6,16 @@ defs files import data from here (from load_data import data)
 
 # this particular example loads data from a pickle file
 
-import cPickle as pickle
+import pickle
 
 data_file = 'data/classification.pkl'
 
-print "loading..."
+print("loading...")
 
 with open( data_file, 'rb' ) as f:
 	data = pickle.load( f )
 
 """
-data is a dict containing numpy arrays: 
+data is a dict containing numpy arrays:
 { 'x_train': x_train, 'y_train': y_train, 'x_test': x_test, 'y_test': y_test }
 """
